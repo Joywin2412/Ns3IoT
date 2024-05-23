849528e88a425753944d5669605a230e84993594
Joywin2412 <joywinpinto1@gmail.om>
2024-05-23 17:34:22 +0530

    Some changes has to be done

--- iot.cc
+++ iot.cc
@@ -17,6 +17,18 @@
 #include "ns3/applications-module.h"
 #include "ns3/ipv4-static-routing-helper.h"
 
+// #include "ns3/lte-helper.h"
+#include "ns3/epc-helper.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/lte-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/point-to-point-helper.h"
+#include "ns3/config-store.h"
+#include "ns3/netanim-module.h"
 #include <algorithm>
 #include <ctime>
 
@@ -135,45 +147,154 @@ int main(int argc, char* argv[])
     macHelper.SetDeviceType(LorawanMacHelper::GW);
     helper.Install(phyHelper, macHelper, gateways);
 
-    Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
+
+    // Lte config
+    // 
+    // 
+
+    uint16_t numberOfNodes = 2;
+  double simTime = 1.1;
+  double distance = 60.0;
+  double interPacketInterval = 100;
+
+  CommandLine cmd;
+  cmd.AddValue("numberOfNodes", "Number of eNodeBs + UE pairs", numberOfNodes);
+  cmd.AddValue("simTime", "Total duration of the simulation [s])", simTime);
+  cmd.AddValue("distance", "Distance between eNBs [m]", distance);
+  cmd.AddValue("interPacketInterval", "Inter packet interval [ms])", interPacketInterval);
+  cmd.Parse(argc, argv);
+
+  Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+  Ptr<PointToPointEpcHelper>  epcHelper = CreateObject<PointToPointEpcHelper> ();
+  lteHelper->SetEpcHelper (epcHelper);
+
+  ConfigStore inputConfig;
+  inputConfig.ConfigureDefaults();
+  cmd.Parse(argc, argv);
+
+  Ptr<Node> pgw = epcHelper->GetPgwNode ();
+
+  NodeContainer remoteHostContainer;
+//  my server
+  remoteHostContainer.Create (1);
+  Ptr<Node> remoteHost = remoteHostContainer.Get (0);
+  InternetStackHelper internet;
+  internet.Install (remoteHostContainer);
+
+  PointToPointHelper p2ph;
+  p2ph.SetDeviceAttribute ("DataRate", DataRateValue (DataRate ("100Gb/s")));
+  p2ph.SetDeviceAttribute ("Mtu", UintegerValue (1500));
+  p2ph.SetChannelAttribute ("Delay", TimeValue (Seconds (0.010)));
+  NetDeviceContainer internetDevices = p2ph.Install (pgw, remoteHost);
+  Ipv4AddressHelper ipv4h;
+  ipv4h.SetBase ("1.0.0.0", "255.0.0.0");
+  Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign (internetDevices);
+  Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
+
+  Ipv4StaticRoutingHelper ipv4RoutingHelper;
+  Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting (remoteHost->GetObject<Ipv4> ());
+  remoteHostStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), 1);
+
+  NodeContainer ueNodes;
+  NodeContainer enbNodes;
+  enbNodes.Create(numberOfNodes);
+  ueNodes.Create(numberOfNodes);
+    // ueNodes.get
+  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
+  for (uint16_t i = 0; i < numberOfNodes; i++)
+  {
+    positionAlloc->Add (Vector(distance * i, 0, 0));
+  }
+  MobilityHelper mobility2;
+  mobility2.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+  mobility2.SetPositionAllocator(positionAlloc);
+  mobility2.Install(enbNodes);
+  mobility2.Install(ueNodes);
+
+  NetDeviceContainer enbLteDevs = lteHelper->InstallEnbDevice (enbNodes);
+  NetDeviceContainer ueLteDevs = lteHelper->InstallUeDevice (ueNodes);
+
+  internet.Install (ueNodes);
+  Ipv4InterfaceContainer ueIpIface;
+  ueIpIface = epcHelper->AssignUeIpv4Address (NetDeviceContainer (ueLteDevs));
+  for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
+  {
+    Ptr<Node> ueNode = ueNodes.Get (u);
+    Ptr<Ipv4StaticRouting> ueStaticRouting = ipv4RoutingHelper.GetStaticRouting (ueNode->GetObject<Ipv4> ());
+    ueStaticRouting->SetDefaultRoute (epcHelper->GetUeDefaultGatewayAddress (), 1);
+  }
+
+  for (uint16_t i = 0; i < numberOfNodes; i++)
+  {
+    lteHelper->Attach (ueLteDevs.Get(i), enbLteDevs.Get(i));
+  }
+
+  uint16_t dlPort = 1234;
+  uint16_t ulPort = 2000;
+  uint16_t otherPort = 3000;
+  ApplicationContainer clientApps;
+  ApplicationContainer serverApps;
+  for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
+  {
+    ++ulPort;
+    ++otherPort;
+    PacketSinkHelper dlPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), dlPort));
+    PacketSinkHelper ulPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), ulPort));
+    PacketSinkHelper packetSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), otherPort));
+    serverApps.Add (dlPacketSinkHelper.Install (ueNodes.Get(u)));
+    serverApps.Add (ulPacketSinkHelper.Install (remoteHost));
+    serverApps.Add (packetSinkHelper.Install (ueNodes.Get(u)));
+
+    UdpClientHelper dlClient (ueIpIface.GetAddress (u), dlPort);
+    dlClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
+    dlClient.SetAttribute ("MaxPackets", UintegerValue(1000000));
+
+    UdpClientHelper ulClient (remoteHostAddr, ulPort);
+    ulClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
+    ulClient.SetAttribute ("MaxPackets", UintegerValue(1000000));
+
+    UdpClientHelper client (ueIpIface.GetAddress (u), otherPort);
+    client.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
+    client.SetAttribute ("MaxPackets", UintegerValue(1000000));
+
+    clientApps.Add (dlClient.Install (remoteHost));
+    clientApps.Add (ulClient.Install (ueNodes.Get(u)));
+    if (u+1 < ueNodes.GetN ())
+    {
+      clientApps.Add (client.Install (ueNodes.Get(u+1)));
+    }
+    else
+    {
+      clientApps.Add (client.Install (ueNodes.Get(0)));
+    }
+  }
+  serverApps.Start (Seconds (0.01));
+  clientApps.Start (Seconds (0.01));
 
 
-   NodeContainer enbNodes;
-   enbNodes.Create(1);
-   NodeContainer ueNodes;
-   ueNodes.Create(2);
-
-   uint16_t port = 9;
-   UdpServerHelper server(port);
-   ApplicationContainer serverApp = server.Install(ueNodes.Get(1));
-   serverApp.Start(Seconds(1.0));
-    serverApp.Stop(Seconds(20.0));
+    
 
 
-    ueNodes.Get(0) = gateways.Get(0);
-    NetDeviceContainer enbDevs;
-    enbDevs = lteHelper->InstallEnbDevice(enbNodes);
 
-    NetDeviceContainer ueDevs;
-    ueDevs = lteHelper->InstallUeDevice(ueNodes);
 
-    lteHelper->Attach(ueDevs, enbDevs.Get(0));
-lteHelper->Attach(ueDevs, enbDevs.Get(1));
 
-    enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
-    EpsBearer bearer(q);
-    lteHelper->ActivateDataRadioBearer(ueDevs, bearer);
+
+
+
+
+
+
+
+
+
+
 
 	
-    /******************
-     * Set Data Rates *
-     ******************/
+    
     std::vector<int> sfQuantity(6);
     sfQuantity = LorawanMacHelper::SetSpreadingFactorsUp(endDevices, gateways, channel);
 
-    /****************
-     *  Simulation  *
-     ****************/
+    AnimationInterface anim ("animation_file.xml");
 
     Simulator::Stop(Hours(2));
 
@@ -183,4 +304,5 @@ lteHelper->Attach(ueDevs, enbDevs.Get(1));
 
     return 0;
 }
+
 
--- simulation.cc
+++ simulation.cc
@@ -4,7 +4,7 @@
 #include "ns3/internet-module.h"
 #include "ns3/lte-module.h"
 #include "ns3/lorawan-module.h"
-
+#include "ns3/netanim-module.h"
 using namespace ns3;
 
 int main(int argc, char *argv[]) {
@@ -12,79 +12,9 @@ int main(int argc, char *argv[]) {
     CommandLine cmd;
     cmd.Parse(argc, argv);
 
-    // Create nodes: IoT devices, a gateway, and a server
-    NodeContainer lorawanNodes;
-    lorawanNodes.Create(10);
-
-    Ptr<Node> gateway = CreateObject<Node>();
-    Ptr<Node> server = CreateObject<Node>();
-
-    // LoRaWAN setup
-    LoRaWANHelper lorawanHelper;
-    lorawanHelper.Install(lorawanNodes);
-
-    // Create LoRaWAN gateway
-    lorawanHelper.InstallGateway(gateway);
-
-    // LTE setup
-    NodeContainer enbNodes;
-    enbNodes.Create(1);
-    
-    NodeContainer ueNodes;
-    ueNodes.Add(gateway);
-
-    // Install LTE modules
-    LteHelper lteHelper;
-    lteHelper.InstallEnbDevice(enbNodes);
-    lteHelper.InstallUeDevice(ueNodes);
-
-    // Install the IP stack on the gateway and the server
-    InternetStackHelper internet;
-    internet.Install(ueNodes);
-    internet.Install(server);
-
-    // Assign IP addresses
-    Ipv4AddressHelper ipv4;
-    ipv4.SetBase("7.0.0.0", "255.0.0.0");
-
-    Ipv4InterfaceContainer enbIpIfaces;
-    enbIpIfaces = ipv4.Assign(lteHelper.GetEnbDevice(enbNodes));
-
-    Ipv4InterfaceContainer ueIpIfaces;
-    ueIpIfaces = ipv4.Assign(lteHelper.GetUeDevice(ueNodes));
-
-    // Connect gateway to the server via Point-to-Point link for simplicity
-    PointToPointHelper p2p;
-    p2p.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
-    p2p.SetChannelAttribute("Delay", StringValue("10ms"));
-
-    NetDeviceContainer p2pDevices;
-    p2pDevices = p2p.Install(gateway, server);
-
-    Ipv4InterfaceContainer p2pInterfaces;
-    p2pInterfaces = ipv4.Assign(p2pDevices);
-
-    // Create Applications
-    uint16_t port = 9;
-
-    // On the server
-    UdpServerHelper udpServer(port);
-    ApplicationContainer serverApp = udpServer.Install(server);
-    serverApp.Start(Seconds(1.0));
-    serverApp.Stop(Seconds(10.0));
-
-    // On the gateway
-    UdpClientHelper udpClient(p2pInterfaces.GetAddress(1), port);
-    udpClient.SetAttribute("MaxPackets", UintegerValue(320));
-    udpClient.SetAttribute("Interval", TimeValue(Seconds(0.05)));
-    udpClient.SetAttribute("PacketSize", UintegerValue(1024));
-
-    ApplicationContainer clientApp = udpClient.Install(gateway);
-    clientApp.Start(Seconds(2.0));
-    clientApp.Stop(Seconds(10.0));
-
     // Enable tracing
-    p2p.EnablePcapAll("iot-lorawan-lte");
+   // p2p.EnablePcapAll("iot-lorawan-lte");
+    AnimationInterface anim("lorawan-animation.xml"); // specify the filename for NetAnim
 
     // Run the simulation
     Simulator::Run();
